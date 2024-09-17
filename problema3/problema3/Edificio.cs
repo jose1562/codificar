a73a5d3f689d8eb1fcc76f40596c247b7600d76d
jose1562 <josetcfc@gmail.com>
2024-09-16 23:34:19 -0500

    Eliminado archivo Edificio y añadido directorio problema3

---
+++ problema3/problema3/Edificio.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace semana_02
+{
+    class Edificio
+    {
+        private int codigo;
+        private int numeroDepartamentos;
+        private int cantidadPisos;
+        private double precioDepartamento;
+
+        public Edificio(int codigo, int numeroDepartamentos, int cantidadPisos, double precioDepartamento)
+        {
+            this.codigo = codigo;
+            this.numeroDepartamentos = numeroDepartamentos;
+            this.cantidadPisos = cantidadPisos;
+            this.precioDepartamento = precioDepartamento;
+        }
+
+        public int Codigo
+        {
+            get { return codigo; }
+            set { codigo = value; }
+        }
+
+        public int NumeroDepartamentos
+        {
+            get { return numeroDepartamentos; }
+            set { numeroDepartamentos = value; }
+        }
+
+        public int CantidadPisos
+        {
+            get { return cantidadPisos; }
+            set { cantidadPisos = value; }
+        }
+
+        public double PrecioDepartamento
+        {
+            get { return precioDepartamento; }
+            set { precioDepartamento = value; }
+        }
+
+        public double PrecioEdificio()
+        {
+            return numeroDepartamentos * precioDepartamento;
+        }
+    }
+}
---
+++ problema3/problema3/Program.cs
@@ -0,0 +1,34 @@
+﻿using semana_02;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Edificio edificio = new Edificio(100,100,230,1000);
+
+        Console.WriteLine("\nPresione cualquier tecla para procesar...");
+        Console.ReadKey();
+
+        Listado(edificio);
+
+        edificio.PrecioDepartamento *= 1.2; // Incremento del 20%
+
+        Console.WriteLine("\nDespués de incrementar el precio en 20%:");
+        Listado(edificio);
+
+        Console.WriteLine("\nPresione cualquier tecla para salir...");
+        Console.ReadKey();
+    }
+
+    static void Listado(Edificio edificio)
+    {
+        Console.WriteLine("\nDatos del Edificio:");
+        Console.WriteLine($"Código: {edificio.Codigo}");
+        Console.WriteLine($"Número de departamentos: {edificio.NumeroDepartamentos}");
+        Console.WriteLine($"Cantidad de pisos: {edificio.CantidadPisos}");
+        Console.WriteLine($"Precio de un departamento: ${edificio.PrecioDepartamento:F2}");
+        Console.WriteLine($"Precio total del edificio: ${edificio.PrecioEdificio():F2}");
+    }
+}
